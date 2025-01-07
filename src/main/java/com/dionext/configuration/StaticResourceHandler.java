c9a979776fba603c7c3e253d9903122c1ef9d604
dionext <dionextsoft@gmail.com>
2025-01-07 16:38:21 +0300

    -

--- src/main/java/com/dionext/configuration/StaticResourceHandler.java
+++ src/main/java/com/dionext/configuration/StaticResourceHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //https://stackoverflow.com/questions/42393211/how-can-i-serve-static-html-from-spring-boot
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 @Slf4j
 public class StaticResourceHandler implements WebMvcConfigurer {
 
@@ -24,18 +24,25 @@ public class StaticResourceHandler implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("====================== StaticResourceHandler ");
         //to do root
         SiteSettings siteSettings = webSitesConfig.getWebsite();
-        for (String folder : siteSettings.getStaticPrefixes()) {
-            //only folders supported, not files
-            String resourceHandler;
-            String resourceLocations;
-            resourceHandler = "/" + folder + "/**";
-            resourceLocations = siteSettings.getSiteStaticStoragePath() + "/" + folder + "/";
-            registry
-                    .addResourceHandler(resourceHandler)
-                    .addResourceLocations(resourceLocations);
-            log.debug("static: " + resourceHandler + " ===> " + resourceLocations);
+        if (siteSettings != null && siteSettings.getStaticPrefixes() != null) {
+            for (String folder : siteSettings.getStaticPrefixes()) {
+                //only folders supported, not files
+                String resourceHandler;
+                String resourceLocations;
+                resourceHandler = "/" + folder + "/**";
+                resourceLocations = siteSettings.getSiteStaticStoragePath() + "/" + folder + "/";
+                registry
+                        .addResourceHandler(resourceHandler)
+                        .addResourceLocations(resourceLocations)
+                        .addResourceLocations("/public" + "/" + folder + "/")
+                        .addResourceLocations("classpath:/static" + "/" + folder + "/");
+                //.setCacheControl(CacheControl.maxAge(Duration.ofDays(365)))
+
+                log.debug("static: " + resourceHandler + " ===> " + resourceLocations);
+            }
         }
 
         //order https://stackoverflow.com/questions/51768263/resourcehandler-conflict-with-wildcards-in-controllers
